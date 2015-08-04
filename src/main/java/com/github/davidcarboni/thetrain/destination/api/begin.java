9371a26d500f548352b5b3d0e3cf7a4d8812e4ed
David Carboni <david@carboni.io>
2015-08-04 14:11:23 +0100

    Data record for timing a single URI transfer.

--- src/main/java/com/github/davidcarboni/thetrain/destination/api/begin.java
+++ src/main/java/com/github/davidcarboni/thetrain/destination/api/begin.java
@@ -1,16 +1,14 @@
 package com.github.davidcarboni.thetrain.destination.api;
 
-import com.github.davidcarboni.cryptolite.Random;
 import com.github.davidcarboni.restolino.framework.Api;
-import com.github.davidcarboni.thetrain.destination.json.DateConverter;
 import com.github.davidcarboni.thetrain.destination.json.Transaction;
+import com.github.davidcarboni.thetrain.destination.storage.Transactions;
 import org.apache.commons.fileupload.FileUploadException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.POST;
 import java.io.IOException;
-import java.util.Date;
 
 /**
  * Created by david on 30/07/2015.
@@ -19,12 +17,8 @@ import java.util.Date;
 public class begin {
 
     @POST
-    public Transaction begin(HttpServletRequest request,
+    public Transaction beginTransaction(HttpServletRequest request,
                                HttpServletResponse response) throws IOException, FileUploadException {
-
-        Transaction transaction = new Transaction();
-        transaction.id = Random.id();
-        transaction.startDate = DateConverter.toString(new Date());
-        return transaction;
+        return Transactions.create();
     }
 }
--- src/main/java/com/github/davidcarboni/thetrain/destination/json/Transaction.java
+++ src/main/java/com/github/davidcarboni/thetrain/destination/json/Transaction.java
@@ -1,9 +1,12 @@
 package com.github.davidcarboni.thetrain.destination.json;
 
+import java.util.List;
+
 /**
  * Created by david on 31/07/2015.
  */
 public class Transaction {
     public String id;
     public String startDate;
+    public List<Timing> uris;
 }
