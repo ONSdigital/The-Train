929c6247fd9944f0dd591a7d4cf14d1d9c55a965
David Carboni <david@carboni.io>
2015-08-06 18:06:08 +0100

    Return type for APIs.

---
+++ src/main/java/com/github/davidcarboni/thetrain/destination/json/Result.java
@@ -0,0 +1,22 @@
+package com.github.davidcarboni.thetrain.destination.json;
+
+/**
+ * Created by david on 31/07/2015.
+ */
+public class Result {
+    public String message;
+    public boolean error;
+    public Transaction transaction;
+
+    /**
+     * The response message returned from the endpoints in this class.
+     * @param message An informational message.
+     * @param error If
+     * @param transaction
+     */
+    public Result(String message, boolean error, Transaction transaction) {
+        this.message = message;
+        this.error = error;
+        this.transaction = transaction;
+    }
+}
