5ed9399dfb1ed5ec5c6e195370b9b23805a99de7
David Carboni <david@carboni.io>
2015-08-06 18:05:09 +0100

    Added configuration for the website directory.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+mkdir target/website
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 java $JAVA_OPTS \
+          -Dthetrain.website=target/website \
           -DPORT=8080 \
           -Drestolino.files=src/main/web \
           -Drestolino.classes=target/classes \
