98304abbad425151baa13c56d5b0bca1bff8d494
David Carboni <david@carboni.io>
2015-08-06 18:06:30 +0100

    Added clone functionality.

--- src/main/java/com/github/davidcarboni/thetrain/destination/json/Transaction.java
+++ src/main/java/com/github/davidcarboni/thetrain/destination/json/Transaction.java
@@ -1,12 +1,48 @@
 package com.github.davidcarboni.thetrain.destination.json;
 
-import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by david on 31/07/2015.
  */
-public class Transaction {
+public class Transaction implements Cloneable {
     public String id;
     public String startDate;
-    public List<Timing> uris;
+    public Set<Timing> uris;
+
+
+    /**
+     * @return A clone of this instance.
+     */
+    public Transaction clone() {
+        try {
+            Transaction transaction = (Transaction) super.clone();
+            transaction.uris = new HashSet<>();
+            for (Timing timing : uris) {
+                transaction.uris.add(timing.clone());
+            }
+            return transaction;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        if (id != null) {
+            result = id.hashCode();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null &&
+                obj.getClass().equals(this.getClass()) &&
+                StringUtils.equals(id, ((Timing) obj).uri);
+    }
 }
