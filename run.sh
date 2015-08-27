6303080744902049f10498cfa1082ad891c3db67
David Carboni <david@carboni.io>
2015-08-27 16:52:07 +0100

    Changed run script to use the jar-with-dependencies, as per the deployed container.

--- run.sh
+++ run.sh
@@ -6,8 +6,16 @@ java $JAVA_OPTS \
           -Dthetrain.website=target/website \
           -Dthetrain.transactions=target/transactions \
           -DPORT=8080 \
-          -Drestolino.files=src/main/web \
-          -Drestolino.classes=target/classes \
           -Drestolino.packageprefix=com.github.davidcarboni.thetrain.api \
-          -cp "target/dependency/*" \
-          com.github.davidcarboni.restolino.Main
+          -jar target/the-train-destination-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+## Reloadable
+#java $JAVA_OPTS \
+#          -Dthetrain.website=target/website \
+#          -Dthetrain.transactions=target/transactions \
+#          -DPORT=8080 \
+#          -Drestolino.files=src/main/web \
+#          -Drestolino.classes=target/classes \
+#          -Drestolino.packageprefix=com.github.davidcarboni.thetrain.api \
+#          -cp "target/dependency/*" \
+#          com.github.davidcarboni.restolino.Main
