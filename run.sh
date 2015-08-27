0a3108ff49b7ec427526dca843033d6145624549
David Carboni <david@carboni.io>
2015-08-27 15:07:10 +0100

    Updated package prefix and configured transactions folder.

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 mkdir target/website
+mkdir target/transactions
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 java $JAVA_OPTS \
           -Dthetrain.website=target/website \
+          -Dthetrain.transactions=target/transactions \
           -DPORT=8080 \
           -Drestolino.files=src/main/web \
           -Drestolino.classes=target/classes \
-          -Drestolino.packageprefix=com.github.davidcarboni.thetrain.destination.api \
+          -Drestolino.packageprefix=com.github.davidcarboni.thetrain.api \
           -cp "target/dependency/*" \
           com.github.davidcarboni.restolino.Main
