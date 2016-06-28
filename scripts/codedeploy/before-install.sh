1ad7da4f8ed9c56804988cbd2e559e36e3984660
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-06-28 12:15:16 +0100

    Fixes before install script
    Remove the first awk command - this was only projecting the image name so when we
    were trying to awk again, we never had more than one output column so the image IDs
    were always blank.

--- scripts/codedeploy/before-install.sh
+++ scripts/codedeploy/before-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONTAINER_ID=$(docker ps -a | grep 'the-train:[[:alnum:]]\{7\}' | awk '{print $1}')
-IMAGE_ID=$(docker images -a | awk '{print $1}' | grep the-train | awk '{print $3}')
+IMAGE_ID=$(docker images -a | grep the-train | awk '{print $3}')
 
 if [[ -n $CONTAINER_ID ]]; then
   docker rm $CONTAINER_ID
