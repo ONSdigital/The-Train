487e2e3b0d6310bfb130343bd188dadf513d7290
Dave Llewellyn <439837@gmail.com>
2018-11-21 09:04:17 +0000

    added unit tests for commit transaction transactionService get transaction error cases

---
+++ src/test/java/com/github/onsdigital/thetrain/routes/CommitTransactionTest.java
@@ -0,0 +1,81 @@
+package com.github.onsdigital.thetrain.routes;
+
+import com.github.onsdigital.thetrain.exception.BadRequestException;
+import com.github.onsdigital.thetrain.exception.PublishException;
+import com.github.onsdigital.thetrain.json.Transaction;
+import com.github.onsdigital.thetrain.service.PublisherService;
+import com.github.onsdigital.thetrain.service.TransactionsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+public class CommitTransactionTest {
+
+    @Mock
+    private Request request;
+
+    @Mock
+    private Response response;
+
+    @Mock
+    private TransactionsService transactionsService;
+
+    @Mock
+    private PublisherService publisherService;
+
+    @Mock
+    private Transaction transaction;
+
+    private Route route;
+    private BadRequestException badRequestException;
+    private PublishException publishException;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        route = new CommitTransaction(transactionsService, publisherService);
+        badRequestException = new BadRequestException("Boom!");
+        publishException = new PublishException("Boom!");
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testTransactionServiceBadRequestException() throws Exception {
+        when(transactionsService.getTransaction(request)).thenThrow(badRequestException);
+
+        try {
+            route.handle(request, response);
+        } catch (BadRequestException e) {
+            assertThat(e.getMessage(), equalTo(badRequestException.getMessage()));
+            verify(transactionsService, times(1)).getTransaction(request);
+            verify(transactionsService, times(1)).update(null);
+            verifyZeroInteractions(publisherService);
+            throw e;
+        }
+    }
+
+    @Test(expected = PublishException.class)
+    public void testTransactionServicePublishException() throws Exception {
+        when(transactionsService.getTransaction(request)).thenThrow(publishException);
+
+        try {
+            route.handle(request, response);
+        } catch (BadRequestException e) {
+            assertThat(e.getMessage(), equalTo(publishException.getMessage()));
+            verify(transactionsService, times(1)).getTransaction(request);
+            verify(transactionsService, times(1)).update(null);
+            verifyZeroInteractions(publisherService);
+            throw e;
+        }
+    }
+}
