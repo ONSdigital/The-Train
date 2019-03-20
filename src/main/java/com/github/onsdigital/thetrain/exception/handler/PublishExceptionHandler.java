08efc738b80fbcadbb3246ae468cee8ffe393612
Dave Llewellyn <439837@gmail.com>
2019-03-20 11:02:22 +0000

    improvements to publish exception logging

--- src/main/java/com/github/onsdigital/thetrain/exception/handler/PublishExceptionHandler.java
+++ src/main/java/com/github/onsdigital/thetrain/exception/handler/PublishExceptionHandler.java
@@ -2,6 +2,7 @@ package com.github.onsdigital.thetrain.exception.handler;
 
 import com.github.onsdigital.thetrain.exception.PublishException;
 import com.github.onsdigital.thetrain.json.Result;
+import com.github.onsdigital.thetrain.logging.TrainEvent;
 import com.google.gson.Gson;
 import spark.ExceptionHandler;
 import spark.Request;
@@ -21,9 +22,13 @@ public class PublishExceptionHandler implements ExceptionHandler<PublishExceptio
     public void handle(PublishException e, Request request, Response response) {
         response.status(e.getStatus());
         response.body(gson.toJson(new Result(e.getMessage(), true, e.getTransaction())));
-        error().transactionID(e.getTransaction().id())
-                .exception(e)
-                .endHTTP(response.raw())
-                .log("publish exception");
+
+        TrainEvent err = error().transactionID(e.getTransaction().id()).endHTTP(response.raw());
+        if (e.getCause() != null) {
+            err.exception(e.getCause());
+        } else {
+            err.exception(e);
+        }
+        err.log("publishing exception: " + e.getMessage());
     }
 }
