6ad20d54610bfacfc256b973d3b68bb8b1c6c9cd
Dave Llewellyn <439837@gmail.com>
2019-03-21 09:27:52 +0000

    added more verbose logging and addition null / empty check to help narrow down issue

--- src/main/java/com/github/onsdigital/thetrain/helpers/FileUploadHelper.java
+++ src/main/java/com/github/onsdigital/thetrain/helpers/FileUploadHelper.java
@@ -3,7 +3,6 @@ package com.github.onsdigital.thetrain.helpers;
 import com.github.davidcarboni.encryptedfileupload.EncryptedFileItemFactory;
 import com.github.onsdigital.thetrain.exception.BadRequestException;
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,12 +27,33 @@ public class FileUploadHelper {
         try {
             InputStream result = null;
 
-            // Set up the objects that do all the heavy lifting
+            info().transactionID(transactionID).log("setting up EncryptedFileItemFactory");
             EncryptedFileItemFactory factory = new EncryptedFileItemFactory();
+
+            info().transactionID(transactionID).log("setting up ServletFileUpload");
             ServletFileUpload upload = new ServletFileUpload(factory);
 
             // Read the items - this will save the values to temp files
-            List<FileItem> fileItemList = upload.parseRequest(request);
+            List<FileItem> fileItemList = null;
+
+            try {
+                info().transactionID(transactionID).log("parsing request");
+                fileItemList = upload.parseRequest(request);
+            } catch (Exception e) {
+                error().exception(e).log("upload.parseRequest(request) threw exception");
+                throw new BadRequestException(e, "upload.parseRequest(request) threw exception (wrapped", transactionID);
+            }
+
+            if (fileItemList == null) {
+                throw new BadRequestException("fileItemsList was null");
+            }
+            if (fileItemList.isEmpty()) {
+                throw new BadRequestException("fileItemsList was empty");
+            }
+
+            info().transactionID(transactionID).data("file_items_size", fileItemList.size())
+                    .log("parsing request successfull");
+
             for (FileItem item : fileItemList) {
                 if (!item.isFormField()) {
                     try {
@@ -41,22 +61,24 @@ public class FileUploadHelper {
                                 .data("content_type", item.getContentType())
                                 .data("field_name", item.getFieldName())
                                 .log("identified fileItem in request body");
+
                         result = item.getInputStream();
                     } catch (IOException e) {
                         throw error().logException(e, "item.getInputStream() threw IO exception");
                     } catch (Exception e) {
-                        throw error().logException(new BadRequestException(e, "", ""),"fileItem.getInputstream  an exception");
+                        throw error().logException(new BadRequestException(e, "fileItem.getInputstream  an exception nested", transactionID),
+                                "fileItem.getInputstream  an exception");
                     }
                 }
             }
 
             if (result == null) {
-                error().data("file_items", fileItemList.stream().map(f -> f.toString()).collect(Collectors.toList())).log("");
+                error().data("file_items", fileItemList.stream().map(f -> f.toString()).collect(Collectors.toList())).log("request body but null check failed");
                 throw new BadRequestException("expected request body but was null");
             }
 
             return result;
-        } catch (IOException | FileUploadException e) {
+        } catch (IOException e) {
             throw new BadRequestException(e, "error while attempting to get inputstream from request body", transactionID);
         }
     }
